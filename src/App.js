a358e5f88516f8648947d00b829c1f03b7f54ea8
suyogmungale <suyogmungale5@gmail.com>
2023-04-13 22:44:42 +0530

    Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+//import { Feed } from '@mui/icons-material';
 
 function App() {
   return (
-  <div>
+    <BrowserRouter>
 
-  </div>
+      <Box sx={{ backgroundColor: '#000' }}>
+        Navbar
+      </Box>
+      <Routes>
+        <Route path='/' exact element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail/>}/>
+        <Route path='/channel/:id' element={<ChannelDetail/>}/>
+        <Route path='/search/:searchTerm' element={<SearchDetail/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
