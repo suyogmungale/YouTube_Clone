5133fe5a9bfd56b3910ef8e5c776ad35053e7747
suyogmungale <suyogmungale5@gmail.com>
2023-04-20 10:20:26 +0530

    videos

--- src/component/VideoCard.jsx
+++ src/component/VideoCard.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Typography, Card, CardContent, CardMedia } from '@mui/material'
+import { CheckCircle } from '@mui/icons-material'
+import {demoThumbnailUrl, demoVideoUrl, demoChannelUrl,demoVideoTitle,} from '../utils/constants'
 
-const VideoCard = () => {
+
+const VideoCard = ({video: {id:{videoId}, snippet}}) => {
   return (
-    <div>VideoCard</div>
+    <Card>
+      <Link to={videoId? `/video/${videoId}` : demoVideoUrl}>
+      <CardMedia image={snippet?.thumbnails?.heigh?.url}
+      alt={snippet.title}
+      sx={{width:358, height:180}}
+      />
+      </Link>
+    </Card>
   )
 }
 
