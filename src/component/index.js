8e6324c78d33fb6e72f1db13020f09f460d498b2
suyogmungale <suyogmungale5@gmail.com>
2023-04-21 17:03:43 +0530

    implementing vite now

--- src/component/index.js
+++
@@ -1,6 +0,0 @@
-export {default as Navbar} from './Navbar'
-export {default as ChannelDetail} from './ChannelDetail'
-export {default as VideoDetail} from './VideoDetail'
-export {default as SearchDetail} from './SearchDetail'
-export {default as Feed} from './Feed'
-export {default as SearchBar} from './SearchBar'
