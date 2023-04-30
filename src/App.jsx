4d4620e3796e102b73071079c4e315a759ccb83b
suyogmungale <suyogmungale5@gmail.com>
2023-04-30 12:58:19 +0530

    nothing to commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Box } from '@mui/material'
 import './index.css'
-
+import {Navbar, Feed, VideoDetails, ChannelDetails, SearchFeed } from './components/index'
 
 
 export const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Box>
+      <Navbar/>
+      <Routes>
+        <Route path='/' exact element={<Feed/>} />
+        <Route path='/video/:id' element={<VideoDetails/>} />
+        <Route path='/channel/:id' element={<ChannelDetails/>} />
+        <Route path='/search/:searchTerm' element={<SearchFeed/>} />
+      </Routes>
+    </Box>
+    </BrowserRouter>
   )
 }
 
