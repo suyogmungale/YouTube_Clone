311376d73405a41f582cbce37856d924cd85e680
suyogmungale <suyogmungale5@gmail.com>
2023-04-30 17:31:12 +0530

    re-writing

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Navbar } from './Navbar'
-export { default as Feed } from './Feed'
-export { default as ChannelDetails } from './ChannelDetails'
-export { default as SearchFeed } from './SearchFeed'
-export { default as VideoDetails } from './videoDetails'
