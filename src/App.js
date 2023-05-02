67636e2b7b097fcd149dcc18b132ef89b1b19838
suyogmungale <suyogmungale5@gmail.com>
2023-05-02 18:31:35 +0530

    header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 
-import { Feed, Header, LeftNav, LeftNavMenu, SearchResultVideoCard, SuggestionVideoCard, SearchResult, VideoCard, VideoDetails } from './componets'
+//import { Feed, Header, LeftNav, LeftNavMenu, SearchResultVideoCard, SuggestionVideoCard, SearchResult, VideoCard, VideoDetails } from './componets'
+
+import Feed from './componets/Feed'
+import Header from './componets/Header'
+import SearchResult from './componets/SearchResult'
+import VideoDetails from './componets/VideoDetails'
 
 import { AppContext, Context } from './context/ContextApi'
 const App = () => {
@@ -11,9 +16,9 @@ const App = () => {
       <div className="flex flex-col h-full">
         <Header/>
         <Routes>
-          <Route/>
-          <Route/>
-          <Route/>
+          <Route path='/' exact element={<Feed/>}/>
+          <Route path='/searchResult/:searchQuery' element={<SearchResult/>}/>
+          <Route path='/video/:id' element={<VideoDetails/>}/>
         </Routes>
       </div>
       </BrowserRouter>
