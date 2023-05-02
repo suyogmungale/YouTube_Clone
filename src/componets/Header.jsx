83c666bda1690689eb00c7d41516735407120571
suyogmungale <suyogmungale5@gmail.com>
2023-05-02 21:24:39 +0530

    implement searchbar

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -11,32 +11,62 @@ import { CgClose } from "react-icons/cg";
 
 import { Context } from "../context/ContextApi";
 import Loader from "../shared/Loader";
-import { useLocation, useNavigate } from 'react-router-dom';
-
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 const Header = () => {
 
-const [searchQuery, setSearchQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
 
-const {loading,mobileMenu,setMobileMenu} = useContext(Context);
+  const { loading, mobileMenu, setMobileMenu } = useContext(Context);
 
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
-const searchQueryHandler = (event) => {
-  if((event?.key === "Enter" || event === "searchButton") && searchQuery.length > 0 ){
-    navigate(`/searchResult/${searchQuery}`);
+  const searchQueryHandler = (event) => {
+    if ((event?.key === "Enter" || event === "searchButton") && searchQuery.length > 0) {
+      navigate(`/searchResult/${searchQuery}`);
+    }
   }
-}
 
-const mobileMenuToggle = () => {
-  setMobileMenu(!mobileMenu);
-};
+  const mobileMenuToggle = () => {
+    setMobileMenu(!mobileMenu);
+  };
 
-const {pathname} = useLocation();
-const pagename = pathname?.split("/")?.filter(Boolean)?.[0]
+  const { pathname } = useLocation();
+  const pagename = pathname?.split("/")?.filter(Boolean)?.[0]
 
   return (
     <div className="sticky top-0 z-10 flex flex-row items-center justify-between h-14 px-4 md:px-5 bg-white dark:bg-black">
+      {loading && <Loader />}
 
+      <div className="flex h-5 items-center">
+        {pagename !== "video" && (
+          <div className="flex md:hidden md:mr-6 cursor-pointer items-center justify-center h-10 w-10 rounded-full hover:bg-[#303030]/[0.6]"
+            onClick={mobileMenuToggle}
+          >
+            (mobileMenu ? {<CgClose className="text-white text-xl" />} :
+            {<SlMenu className="text-white text-xl" />})
+          </div>
+        )}
+        <Link to="/" className=" flex h-5 items-center">
+        <img className="h-full hidden dark:md:block " src={ytLogo} alt="ytl" />
+        <img className="h-full md:hidden " src={ytLogoMobile} alt="ytm" />
+        </Link>
+        <div className="group flex items-center">
+          <div className="flex h-8 md:h-10 md:ml-10 md:pl-5 border border-[#303030] rounded-l-3xl group-focus-within:border-blue-500 md:group-focus-within:ml-5 md:group-focus-within:pl-0">
+            <div className="w-10 items-center justify-center hidden group-focus-within:md:flex">
+              <IoIosSearch className="text-white text-xl"/>
+            </div>
+            <input
+            type="text"
+            className="bg-transparent outline-none text-white pr-5 pl-5 md:pl-0 w-44 md:group-focus-within:pl-0 md:w-64 lg:w-[500px]"
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onKeyUp={searchQueryHandler}
+            placeholder="Search"
+            value={searchQuery}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
